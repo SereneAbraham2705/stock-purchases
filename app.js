21fdcb420ea7bf7b54dfbd81c1634cc21d383e7a
SereneAbraham2705 <84651620+SereneAbraham2705@users.noreply.github.com>
2022-10-20 09:44:08 +0530

    Validate the input field

--- app.js
+++ app.js
@@ -9,15 +9,18 @@ function calculateProfitAndLoss(initial,quantity,current){
         const loss=(initial-current)*quantity;
         const losspercentage=(loss/initial)*100;
         //const resans1=toFixed(losspercentage);
+        outputbox.style.color="red";
         outputbox.innerText="Hey the loss is "+loss+" and the loss percentage is "+losspercentage+" %";
     }
     else if(initial<current){
         const profit=(current-initial)*quantity;
         const profitpercentage=(profit/initial)*100;
         //const resans2=toFixed(profitpercentage);
-        outputbox.innerText="Hey the loss is "+profit+" and the loss percentage is "+profitpercentage+" %";
+        outputbox.style.color="green";
+        outputbox.innerText="Hey the profit is "+profit+" and the profit percentage is "+profitpercentage+" %";
     }
     else{
+        outputbox.style.color="blue";
         outputbox.innerText="Neither Profit nor Loss!";
     }
 
@@ -27,7 +30,18 @@ function outputHandler(){
     const ip=initialprice.value;
     const quan=stocksquantity.value;
     const cur=currentprice.value;
-    calculateProfitAndLoss(ip,quan,cur);
+    if(ip=="" || quan=="" || cur==""){
+        outputbox.style.color="red";
+        outputbox.innerText="Please fill the empty fields."
+    }
+    else if(ip<=0 || quan<=0 || cur<=0){
+        outputbox.style.color="red";
+        outputbox.innerText="Enter values greater than 0."
+    }
+    else{
+        calculateProfitAndLoss(ip,quan,cur);
+    }
+    
     //OUTPUT HANDLED Comment
 }
 resultbtn.addEventListener('click',outputHandler);
